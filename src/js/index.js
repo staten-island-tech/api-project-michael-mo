4ee5dfb9b3ec53ed0dd9d45b07182a1d5c6318d3
YoinkSpaghetti <71717533+YoinkSpaghetti@users.noreply.github.com>
2021-05-14 09:59:54 -0400

    committed at school

--- src/js/index.js
+++ src/js/index.js
@@ -7,7 +7,7 @@ const query = async function () {
   try {
     const response = await fetch(key);
     const data = await response.json();
-    console.log(data.results[0].name);
+    data.results.forEach(element => document.body.appendChild(element.name));
   } catch (error) {
     console.log(error);
   }
